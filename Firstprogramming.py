4d31bbe20e48eb6d827fbef3fb21f831e5d7ab86
princisoni534-ai <princisoni534@gmail.com>
2026-02-10 11:59:54 +0530

    Add files via upload

---
+++ Firstprogramming.py
@@ -0,0 +1,176 @@
+print("Hello World")
+#simple codes 
+# print("my name is princi soni")
+# print("my age is 19")
+# print("my name is princi soni","my age is 19")
+# print(24)
+# print(34)
+# print(23+34)
+
+#REVISION
+# print("Hello World")
+# print("princi soni")
+# print("my name is princi soni","my age is 19")
+# print(25+36)
+# print(35-2)
+# print(3*3)
+# print(8/4)
+# a=5
+# b=3
+# print(a+b)
+# print(a*b)
+
+# #Variables=a variables is a given name to a memmory location of a program.
+#Examples:
+# name="Princi Soni" #string
+# age=19 #integer
+# price=25.6 #float
+# print(age)#print(name) , print(age) , print(price)
+# #..
+# name="lakhan lal soni"
+# age=55
+# price=44.6
+# print("my age is:",age)
+# print("my name is:",name)
+# print("price is:",price)
+# #..
+# age2=age
+# print(age2)
+# #..
+# name2=name
+# print(name2)
+# #..
+# print(type(name))
+# print(type(age))
+# print(type(price))
+
+
+# #Data type = Data types tells python what kinds of data we are working with.(five types)
+
+# # 1.String=a sequence of a character(enclosed by single' or double" quotes)
+# #Example:
+# name1="siya"
+# name2='siya'
+# name3='''siya'''
+# print(name1)
+# print(name2)
+# print(name3)
+
+# #2.Boolean= only two values(True or False) ,  Decision making
+# #Example:
+# name="riddhi"
+# age=True
+# print(type(age))
+
+# #3.Float= only decimal points
+# #Example:
+# a=23
+# b=34.5
+# print(type(b))
+
+# #4.Integer = whole numbes (no decimal points)
+# #Example:
+# a=9
+# print(type(a))
+
+# #5.None
+# a=None
+# print(type(a))
+
+
+# #PRINT SUM
+# a=4
+# b=5
+# sum=a+b
+# print(sum)
+
+# diff=a-b
+# print(diff)
+
+# multiply=a*b
+# print(multiply)
+
+# divide=a/b
+# print(divide)
+
+# #comments
+# '''Hello!
+# my name is princi soni 
+# my father name is lakhanlal soni 
+# (he is government teacher)'''
+
+# # print("Hello World")
+# # print("Hello World")
+# # print("Hello World")
+
+
+# #OPERATORS=An opertor is a operators that used to perform operands on data value, such as calculation,comparison,or logical decision.
+# #types=Arithemetic,comparison,logical,bitwise,assignment,identity,membership.
+
+# #1.Arithematic operators
+# a=2
+# b=6
+# sum=a+b
+# print(sum)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a%b) #reminder
+# print(a**b) #a power 1 
+
+# #Camparison operators
+# a=40
+# b=30
+# print(a == b) #False
+# print(a != b) #True
+# print(a < b) #False
+# print(a <= b ) #False
+# print(a >= b) #True
+# print(a>b) #True
+
+# #Assignment operators
+# num=10
+# num+=10
+# print(num) #20
+
+# num=10
+# num-=10
+# print(num) #0
+
+# num=20
+# num*=20
+# print(num) #400
+
+# num=300
+# num/=3
+# print(num) #100.0
+
+# num=30
+# num%=3
+# print(num) #Remainder 0
+# print("num:",num) #num:0
+
+# num=2
+# num**=3
+# print("num:",num) #num:8
+
+# #Logical operators
+# val1=True
+# val2=True
+# print(val1 and val2) #True
+# print("operator:",val1 and val2) #jab dono value True ho tabhi ans true aata h
+
+# num=False
+# num1=False
+# print("operators:",num or num1) #jab dono operands me false hoga tabhi ans false ayega
+
+
+
+
+
+
+
+
+
+
+
